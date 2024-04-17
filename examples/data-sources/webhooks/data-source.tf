b2b0491b5b9ebe11b2425ebd5c33467294dbd740
Tolga Akyazi <tolga@weave.nl>
2024-04-17 09:38:07 +0200

    chore: add example data-source.tf

---
+++ examples/data-sources/webhooks/data-source.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    adyen = {
+      source = "hashicorp.com/edu/adyen"
+    }
+  }
+}
+
+provider "adyen" {
+  api_key = "API_KEY"
+  environment = "test" // "live"
+  merchant_account = "WeaveAccountECOM"
+  company_account = "WeaveAccount"
+}
+
+data "adyen_webhooks_merchant" "example" {}
+
+output "adyen_webhooks_merchant" {
+  value = data.adyen_webhooks_merchant.example
+}
+
